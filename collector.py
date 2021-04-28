22bb5166ef6fe9fb88e5bb4995f2f79d2e5df195
Smolnaya <57458068+Smolnaya@users.noreply.github.com>
2021-04-28 16:27:29 +0300

    1.2

--- collector.py
+++ collector.py
@@ -1,11 +1,29 @@
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
-
 from Article import Article
 
+import concurrent.futures
 
-def collectArticles(articleNumber):
+THREAD_QUANTITY = 4
+
+
+def getArticles(articleNumber):
+    articles = list()
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for i in range(THREAD_QUANTITY):
+            thread = executor.submit(collectArticles, articleNumber, i)
+        val = thread.result()
+        articles.extend(val)
+    return articles
+
+
+def splitList(fullList, wanted_parts=THREAD_QUANTITY):
+    length = len(fullList)
+    return [fullList[i * length // wanted_parts: (i + 1) * length // wanted_parts]
+            for i in range(wanted_parts)]
+
+
+def collectArticles(articleNumber, param):
     url = 'https://the-geek.ru/category/news'
     driver = webdriver.Chrome()
     driver.implicitly_wait(10)
@@ -13,38 +31,40 @@ def collectArticles(articleNumber):
     assert 'Новости' in driver.title
 
     articleElem = list()
-    articleList = list()
     while len(articleElem) < articleNumber:
         html = driver.find_element_by_tag_name('html')
         html.send_keys(Keys.END)
         articleElem = driver.find_elements_by_xpath('//article')
+    print('articleElem: ', len(articleElem))
 
-    for article in articleElem:
-        href = driver.find_element_by_xpath(
-            f"//article[@id='{article.get_attribute('id')}']/a").get_attribute('href')
+    parts = splitList(articleElem)
+    lst = parts[param]
+
+    articleList = list()
+    for article in lst:
+        href = driver.find_element_by_xpath(f"//article[@id='{article.get_attribute('id')}']/a") \
+            .get_attribute('href')
         driver.execute_script(f"window.open('{href}', 'new_window')")
         driver.switch_to.window(driver.window_handles[1])
-        title = driver.find_element_by_xpath("//article/header/h1").text
-        date = driver.find_element_by_xpath(f"//article/div/div/a[@href='{href}']").text
-        author = driver.find_element_by_xpath(f"//article/div/div/a[@href!='{href}']").text
-        textList = driver.find_elements_by_xpath("//article/div/div/p")
-        text = ''
-        for elem in textList:
-            text += elem.text + '\n'
-        tags = ''
-        source = ''
         try:
-            source = driver.find_element_by_xpath("//article/div[3]/div[2]/div/div/div/a").text
+            title = driver.find_element_by_xpath("//article/header/h1").text
+            date = driver.find_element_by_xpath(f"//article/div/div/a[@href='{href}']").text
+            author = driver.find_element_by_xpath(f"//article/div/div/a[@href!='{href}']").text
+            source = driver.find_element_by_xpath("//article/div/div/div/div/div/a").text
+            textList = driver.find_elements_by_xpath("//article/div/div/p")
             tagList = driver.find_elements_by_xpath("//article/div[3]/div[3]/a")
+            text = ''
+            tags = ''
+            for elem in textList:
+                text += elem.text + '\n'
             for elem in tagList:
                 tags += elem.text + '\n'
-        except NoSuchElementException:
-            pass
-
-        articleList.append(Article(title, date, author, text.strip(), tags.strip(), source))
-
-        driver.close()
-        driver.switch_to.window(driver.window_handles[0])
+            articleList.append(Article(title, date, author, text.strip(), tags.strip(), source))
+        except Exception as err:
+            print(f'Exception: {err}')
+        finally:
+            driver.close()
+            driver.switch_to.window(driver.window_handles[0])
 
     driver.close()
     return articleList
--- start.py
+++ start.py
@@ -1,7 +1,26 @@
+import time
 import collector
 import xmlGenerator
+import datetime
+import os
 
 if __name__ == "__main__":
-    articleList = collector.collectArticles(1000)
+    start_time = time.time()
+
+    # Количество потоков в THREAD_QUANTITY (collector.py)
+
+    # Количество статей
+    articleList = collector.getArticles(100)
+
+    print('collectArticles ----   %s  ----',
+          str(datetime.timedelta(seconds=(time.time() - start_time))))
+    start_time2 = time.time()
+
+    # Создать файлы xml в папке 'files'
     for article in articleList:
         xmlGenerator.generateXml(article)
+
+    print('generateXml ----   %s  ----',
+          str(datetime.timedelta(seconds=(time.time() - start_time2))))
+
+    print('xml: ', len(os.listdir('files')))
