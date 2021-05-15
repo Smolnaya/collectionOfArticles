2ac75f7b0ae36b14a118f0aede48cadc8d60a2e3
Smolnaya <57458068+Smolnaya@users.noreply.github.com>
2021-05-15 13:02:40 +0300

    1.3

--- xmlGenerator.py
+++ xmlGenerator.py
@@ -8,23 +8,41 @@ def generateXml(article: Article):
 
     sourceElem = et.SubElement(root, 'source')
     sourceElem.text = article.source
+    sourceElem.attrib['verify'] = "true"
+    sourceElem.attrib['type'] = "str"
+    sourceElem.attrib['auto'] = "true"
 
     # categoryElem = et.SubElement(root, 'category')
 
     authorElem = et.SubElement(root, 'author')
     authorElem.text = article.author
+    authorElem.attrib['verify'] = "true"
+    authorElem.attrib['type'] = "str"
+    authorElem.attrib['auto'] = "true"
 
     titleElem = et.SubElement(root, 'title')
     titleElem.text = article.title
+    titleElem.attrib['verify'] = "true"
+    titleElem.attrib['type'] = "str"
+    titleElem.attrib['auto'] = "true"
 
     dateElem = et.SubElement(root, 'date')
     dateElem.text = service.convertDate(article.date)
+    dateElem.attrib['verify'] = "true"
+    dateElem.attrib['type'] = "date"
+    dateElem.attrib['auto'] = "true"
 
     tagsElem = et.SubElement(root, 'tags')
     tagsElem.text = article.tags
+    tagsElem.attrib['verify'] = "true"
+    tagsElem.attrib['type'] = "str"
+    tagsElem.attrib['auto'] = "true"
 
     textElem = et.SubElement(root, 'text')
     textElem.text = f"![CDATA[{article.text}]]"
+    textElem.attrib['verify'] = "true"
+    textElem.attrib['type'] = "str"
+    textElem.attrib['auto'] = "true"
 
     tree = et.ElementTree(root)
 
